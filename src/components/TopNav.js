144e7b7ea7eef705d5ed6d7d02fffacb7a9f54f9
RohanDamani <damanirohan@gmail.com>
2018-11-14 18:36:25 -0800

    Add Roventures logo

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Header, Icon } from 'react-native-elements';
 import { updatePhotoViewerAlbum, clearAlbumData } from '../actions';
+import { Image, View } from 'react-native';
 
 class TopNav extends React.Component {
   render() {
@@ -11,44 +12,34 @@ class TopNav extends React.Component {
       clearAlbumData,
       bottomNavActiveTab,
     } = this.props;
-    if (photoViewerAlbum && bottomNavActiveTab === 'photos') {
-      return (
-        <Header
-          centerComponent={{
-            text: photoViewerAlbum,
-            style: { color: 'aqua', fontSize: 24 },
-          }}
-          leftComponent={
+    return (
+      <Header
+        centerComponent={
+          <Image
+            source={require('../roventures_logo_mobile.png')}
+            style={{ width: 220, height: 35 }}
+          />
+        }
+        leftComponent={
+          photoViewerAlbum && bottomNavActiveTab === 'photos' ? (
             <Icon
               name="ios-arrow-back"
               color="aqua"
               type="ionicon"
               size={35}
-              containerStyle={{ marginTop: 28 }}
+              containerStyle={{ marginTop: 28, width: 28 }}
               onPress={() => {
                 updatePhotoViewerAlbum('');
                 clearAlbumData();
               }}
             />
-          }
-          outerContainerStyles={{
-            height: 90,
-            backgroundColor: '#333',
-            borderBottomColor: '#777',
-          }}
-        />
-      );
-    }
-    return (
-      <Header
-        centerComponent={{
-          text: 'RoVentures',
-          style: { color: 'aqua', fontSize: 24 },
-        }}
+          ) : null
+        }
         outerContainerStyles={{
           height: 90,
           backgroundColor: '#333',
           borderBottomColor: '#777',
+          paddingBottom: 7,
         }}
       />
     );
